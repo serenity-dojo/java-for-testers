1def99bcfef30b759852e9e0e28c5ecdb06b4fc4
John Ferguson Smart <john.smart@wakaleo.com>
2020-04-20 11:45:53 +0100

    Sample solution

--- src/test/java/com/serenitydojo/WhenCreatingObjects.java
+++ src/test/java/com/serenitydojo/WhenCreatingObjects.java
@@ -18,10 +18,10 @@ public class WhenCreatingObjects {
     @Test
     public void creating_a_dog() {
 
-//        Dog dog = new Dog("Fido","Bone", 5);
-//        Assert.assertEquals(fido.getName(), "Fido");
-//        Assert.assertEquals(fido.getFavoriteToy, "Bone");
-//        Assert.assertEquals(fido.getAge(), 5);
+        Dog fido = new Dog("Fido","Bone", 5);
 
+        Assert.assertEquals(fido.getName(), "Fido");
+        Assert.assertEquals(fido.getFavoriteToy(), "Bone");
+        Assert.assertEquals(fido.getAge(), 5);
     }
 }
