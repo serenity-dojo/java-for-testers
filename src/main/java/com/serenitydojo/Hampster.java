86cef35434eb3b6aebf63ef0b0aacbffa5f7c638
John Ferguson Smart <john.smart@wakaleo.com>
2020-04-22 09:34:26 +0100

    Sample solution

---
+++ src/main/java/com/serenitydojo/Hampster.java
@@ -0,0 +1,20 @@
+package com.serenitydojo;
+
+public class Hampster extends Pet {
+    private String favoriteGame;
+    private int age;
+
+    public Hampster(String name, String favoriteGame, int age) {
+        super(name);
+        this.favoriteGame = favoriteGame;
+        this.age = age;
+    }
+
+    public String getFavoriteGame() {
+        return favoriteGame;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
