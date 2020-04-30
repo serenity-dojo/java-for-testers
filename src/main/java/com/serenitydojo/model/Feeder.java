1b42e3496459df7d917c720a9333345af3da99bf
John Ferguson Smart <john.smart@wakaleo.com>
2020-04-30 14:40:07 +0100

    Exercise starting point

--- src/main/java/com/serenitydojo/model/Feeder.java
+++ src/main/java/com/serenitydojo/model/Feeder.java
@@ -1,13 +1,17 @@
 package com.serenitydojo.model;
 
 public class Feeder {
-    public String feeds(String animal, boolean isPremium) {
-        if (animal.equals("Cat")) {
-            return (isPremium) ? "Salmon" : "Tuna";
-        } else if (animal.equals("Dog")) {
-            return (isPremium) ? "Deluxe Dog Food" : "Dog Food";
-        } else {
-            return (isPremium) ? "Lettuce" : "Cabbage";
+    public String feeds(AnimalType animal, boolean isPremium) {
+
+        switch (animal) {
+            case CAT:
+                return (isPremium) ? "Salmon" : "Tuna";
+            case DOG:
+                return  (isPremium) ? "Deluxe Dog Food" : "Dog Food";
+            case HAMSTER:
+                return (isPremium) ? "Lettuce" : "Cabbage";
+            default:
+                return  "Don't know this animal - don't know the food";
         }
     }
 }
