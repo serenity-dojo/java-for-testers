f762903134b97527fde3d576789e4b1e5a3ecd28
John Ferguson Smart <john.smart@wakaleo.com>
2020-04-30 11:34:05 +0100

    Sample solution

--- src/test/java/com/serenitydojo/WhenFeedingTheAnimals.java
+++ src/test/java/com/serenitydojo/WhenFeedingTheAnimals.java
@@ -40,6 +40,24 @@ public class WhenFeedingTheAnimals {
         String food = feeder.feeds("Cat", true);
 
         Assert.assertEquals("Salmon", food);
-
     }
+
+    @Test
+    public void shouldFeedPremiumDogsDeluxeDogFood() {
+        Feeder feeder = new Feeder();
+
+        String food = feeder.feeds("Dog", true);
+
+        Assert.assertEquals("Deluxe Dog Food", food);
+    }
+
+    @Test
+    public void shouldFeedPremiumHamstersLettuce() {
+        Feeder feeder = new Feeder();
+
+        String food = feeder.feeds("Hamster", true);
+
+        Assert.assertEquals("Lettuce", food);
+    }
+
 }
