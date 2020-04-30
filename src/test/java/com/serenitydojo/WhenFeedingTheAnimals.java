a89ca62c31ced7e9879a01fbaa59467179246c59
John Ferguson Smart <john.smart@wakaleo.com>
2020-04-30 14:40:07 +0100

    Exercise starting point

--- src/test/java/com/serenitydojo/WhenFeedingTheAnimals.java
+++ src/test/java/com/serenitydojo/WhenFeedingTheAnimals.java
@@ -1,8 +1,11 @@
 package com.serenitydojo;
 
+import com.serenitydojo.model.AnimalType;
 import com.serenitydojo.model.Feeder;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static com.serenitydojo.model.AnimalType.*;
 
 public class WhenFeedingTheAnimals {
 
@@ -10,7 +13,7 @@ public class WhenFeedingTheAnimals {
     public void shouldFeedCatsTuna() {
         Feeder feeder = new Feeder();
 
-        String food = feeder.feeds("Cat", false);
+        String food = feeder.feeds(CAT, false);
 
         Assert.assertEquals("Tuna", food);
     }
@@ -19,7 +22,7 @@ public class WhenFeedingTheAnimals {
     public void shouldFeedHamstersCabbage() {
         Feeder feeder = new Feeder();
 
-        String food = feeder.feeds("Hamster", false);
+        String food = feeder.feeds(HAMSTER, false);
 
         Assert.assertEquals("Cabbage", food);
     }
@@ -28,7 +31,7 @@ public class WhenFeedingTheAnimals {
     public void shouldFeedDogsDogFood() {
         Feeder feeder = new Feeder();
 
-        String food = feeder.feeds("Dog", false);
+        String food = feeder.feeds(DOG, false);
 
         Assert.assertEquals("Dog Food", food);
     }
@@ -37,7 +40,7 @@ public class WhenFeedingTheAnimals {
     public void shouldFeedPremiumCatsPremiumFood() {
         Feeder feeder = new Feeder();
 
-        String food = feeder.feeds("Cat", true);
+        String food = feeder.feeds(CAT, true);
 
         Assert.assertEquals("Salmon", food);
     }
@@ -46,7 +49,7 @@ public class WhenFeedingTheAnimals {
     public void shouldFeedPremiumDogsDeluxeDogFood() {
         Feeder feeder = new Feeder();
 
-        String food = feeder.feeds("Dog", true);
+        String food = feeder.feeds(DOG, true);
 
         Assert.assertEquals("Deluxe Dog Food", food);
     }
@@ -55,7 +58,7 @@ public class WhenFeedingTheAnimals {
     public void shouldFeedPremiumHamstersLettuce() {
         Feeder feeder = new Feeder();
 
-        String food = feeder.feeds("Hamster", true);
+        String food = feeder.feeds(HAMSTER, true);
 
         Assert.assertEquals("Lettuce", food);
     }
