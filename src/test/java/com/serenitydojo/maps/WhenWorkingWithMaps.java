6110541d0e5a44d30cc6c18494207d3e863954f7
John Ferguson Smart <john.smart@wakaleo.com>
2020-06-02 08:09:49 +0100

    Sample map code

--- src/test/java/com/serenitydojo/maps/WhenWorkingWithMaps.java
+++ src/test/java/com/serenitydojo/maps/WhenWorkingWithMaps.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class WhenWorkingWithMaps {
 
@@ -17,5 +19,43 @@ public class WhenWorkingWithMaps {
 
         Integer numberOfRedBalls = numberOfBallsByColor.get("red");
         System.out.println("There are "  + numberOfRedBalls + " red balls");
+    }
+
+    @Test
+    public void creatingAMapMoreConcisely() {
+        Map<String, Integer> numberOfBallsByColor = Map.of("red",3,"green", 6,"blue", 5);
+
+        Map<String,Integer> moreColors = new HashMap<>();
+        moreColors.put("yellow", 10);
+        moreColors.put("mauve", 0);
+        moreColors.putAll(numberOfBallsByColor);
+
+        moreColors.put("yellow", 20);
+        moreColors.replace("pink", 15);
+
+        System.out.println("Number of pink tennis balls " + moreColors.getOrDefault("pink",0));
+    }
+
+    @Test
+    public void iteratingOverMapKeys() {
+        Map<String, Integer> numberOfBallsByColor = Map.of("red",3,"green", 6,"blue", 5);
+
+        System.out.println("Keys = " + numberOfBallsByColor.keySet());
+
+        printMap(numberOfBallsByColor);
+
+        Map<String, Integer> modifiableMap = new HashMap<>();
+        modifiableMap.putAll(numberOfBallsByColor);
+
+        modifiableMap.clear();
+
+        printMap(modifiableMap);
+
+    }
+
+    private void printMap(Map<String, Integer> numberOfBallsByColor) {
+        for(Map.Entry<String, Integer> entry : numberOfBallsByColor.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
     }
 }
