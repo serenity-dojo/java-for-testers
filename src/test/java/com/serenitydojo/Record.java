a9baf3595d97dd762be3440858573148f8494aba
John Ferguson Smart <john.smart@wakaleo.com>
2020-06-30 17:47:34 +0100

    Sample code

--- src/test/java/com/serenitydojo/Record.java
+++
@@ -1,5 +0,0 @@
-package com.serenitydojo;
-
-import java.util.List;
-
-record Order(String id, String name, int quanity, double itemPrice, double totalPrice, List<String>tags) {}
---
+++ src/test/java/com/serenitydojo/exceptions/WhenWorkingWithExceptions.java
@@ -0,0 +1,37 @@
+package com.serenitydojo.exceptions;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WhenWorkingWithExceptions {
+
+    @Test
+    public void shouldShowTheLengthOfAString() {
+        StringProcessor stringProcessor = new StringProcessor();
+
+        String result = stringProcessor.showLengthOf("some string");
+
+        assertThat(result).isEqualTo("some string has a length of 11");
+    }
+
+    @Test
+    public void shouldShowZeroForNullStrings() {
+
+        StringProcessor stringProcessor = new StringProcessor();
+
+        String result = stringProcessor.showLengthOf(null);
+
+        assertThat(result).isEqualTo("null has a length of 0");
+    }
+
+    @Test(expected = TestEnvironmentUnavailableException.class)
+    public void shouldFindThePort() {
+            StringProcessor stringProcessor = new StringProcessor();
+
+            stringProcessor.getPortOf("A:https://www.google.com");
+    }
+}
