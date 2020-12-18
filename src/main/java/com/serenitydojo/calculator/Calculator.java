b48c0acfde5db9860f1e170b59dd10c9d9979c40
Lloyd Blake <treleven.lloyd@gmail.com>
2020-12-18 17:56:51 +0000

    completed challenge and added some division tests

--- src/main/java/com/serenitydojo/calculator/Calculator.java
+++ src/main/java/com/serenitydojo/calculator/Calculator.java
@@ -1,7 +1,45 @@
 package com.serenitydojo.calculator;
 
+import com.google.common.base.Splitter;
+
+import java.util.List;
+
+import static org.apache.commons.lang3.StringUtils.isNumeric;
+
 public class Calculator {
     public int evaluate(String expression) {
-        return 0;
+        if (expression.isEmpty()) {
+            return 0;
+        }
+        List<String> tokens = Splitter.on(" ").splitToList(expression);
+
+        int runningTotal = 0;
+        String nextOperator = "+";
+
+        for(String token : tokens) {
+            if (!isNumeric(token)) {
+                nextOperator = token;
+            } else {
+                runningTotal = process(runningTotal, nextOperator, token);
+            }
+        }
+
+        return runningTotal;
     }
+
+    private int process(int runningTotal, String nextOperator, String token) {
+        switch (nextOperator) {
+            case "+":
+                return runningTotal + Integer.parseInt(token);
+            case "-":
+                return runningTotal - Integer.parseInt(token);
+            case "*":
+                return runningTotal * Integer.parseInt(token);
+            case "/":
+                return runningTotal / Integer.parseInt(token);
+            default:
+                throw new IllegalMathsOperatorException("Unknown operator " + nextOperator);
+        }
+    }
+
 }
