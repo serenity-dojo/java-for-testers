f03d828abef53c817a03edf41eda7322b6d83523
Francesco Calvino <fc572@users.noreply.github.com>
2021-06-04 07:27:18 +0100

    fc solution not elegant, but works

--- src/test/java/com/serenitydojo/calculator/WhenDoingMaths.java
+++ src/test/java/com/serenitydojo/calculator/WhenDoingMaths.java
@@ -1,6 +1,6 @@
 package com.serenitydojo.calculator;
 
-import org.apache.commons.lang3.StringUtils;
+import com.serenitydojo.exceptions.IllegalMathsOperatorException;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,38 +13,43 @@ public class WhenDoingMaths {
         assertThat(calculator.evaluate("")).isEqualTo(0);
     }
 
-//    @Test(expected = IllegalMathsOperatorException.class)
-//    public void shouldReportNonSupportedOperations() {
-//        calculator.evaluate("1 ^ 2");
-//    }
-//
-//    @Test
-//    public void shouldReturnTheValueOfASingleNumber() {
-//        assertThat(calculator.evaluate("1")).isEqualTo(1);
-//    }
-//
-//    @Test
-//    public void shouldAddTwoNumbers() {
-//        assertThat(calculator.evaluate("1 + 1")).isEqualTo(2);
-//    }
-//
-//    @Test
-//    public void shouldAddThreeNumbers() {
-//        assertThat(calculator.evaluate("1 + 2 + 3")).isEqualTo(6);
-//    }
-//
-//    @Test
-//    public void shouldAlsoSubtract() {
-//        assertThat(calculator.evaluate("10 - 6")).isEqualTo(4);
-//    }
-//
-//    @Test
-//    public void shouldAddAndSubtract() {
-//        assertThat(calculator.evaluate("10 + 5 - 6")).isEqualTo(9);
-//    }
-//
-//    @Test
-//    public void shouldMultiplyNumbers() {
-//        assertThat(calculator.evaluate("10 * 5")).isEqualTo(50);
-//    }
+    @Test(expected = IllegalMathsOperatorException.class)
+    public void shouldReportNonSupportedOperations() {
+        calculator.evaluate("1 ^ 2");
+    }
+
+    @Test
+    public void shouldReturnTheValueOfASingleNumber() {
+        assertThat(calculator.evaluate("1")).isEqualTo(1);
+    }
+
+    @Test
+    public void shouldAddTwoNumbers() {
+        assertThat(calculator.evaluate("1 + 1")).isEqualTo(2);
+    }
+
+    @Test
+    public void shouldAddThreeNumbers() {
+        assertThat(calculator.evaluate("1 + 2 + 3")).isEqualTo(6);
+    }
+
+    @Test
+    public void shouldAlsoSubtract() {
+        assertThat(calculator.evaluate("10 - 6")).isEqualTo(4);
+    }
+
+    @Test
+    public void shouldAddAndSubtract() {
+        assertThat(calculator.evaluate("10 + 5 - 6")).isEqualTo(9);
+    }
+
+    @Test
+    public void shouldMultiplyNumbers() {
+        assertThat(calculator.evaluate("10 * 5")).isEqualTo(50);
+    }
+
+    @Test
+    public void shouldAddAndSubtractLongExpression(){
+        assertThat(calculator.evaluate("1 + 2 + 3 - 6 + 2")).isEqualTo(2);
+    }
 }
