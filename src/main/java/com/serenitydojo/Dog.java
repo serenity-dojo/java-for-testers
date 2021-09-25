5ea631245678a77d161bbd1074d47513ac317d59
Karol <krl.michniewicz@gmail.com>
2021-09-25 07:33:39 +0200

    dog class created

---
+++ src/main/java/com/serenitydojo/Dog.java
@@ -0,0 +1,25 @@
+package com.serenitydojo;
+
+public class Dog {
+    private String name;
+    private String favoriteToy;
+    private int age;
+
+    public Dog(String name, String favoriteToy, int age) {
+        this.name = name;
+        this.favoriteToy = favoriteToy;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFavoriteToy() {
+        return favoriteToy;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
--- src/test/java/com/serenitydojo/WhenCreatingObjects.java
+++ src/test/java/com/serenitydojo/WhenCreatingObjects.java
@@ -18,10 +18,9 @@ public class WhenCreatingObjects {
     @Test
     public void creating_a_dog() {
 
-//        Dog dog = fido Dog("Fido","Bone", 5);
-//        Assert.assertEquals(fido.getName(), "Fido");
-//        Assert.assertEquals(fido.getFavoriteToy(), "Bone");
-//        Assert.assertEquals(fido.getAge(), 5);
-
+        Dog fido = new Dog("Fido","Bone", 5);
+        Assert.assertEquals(fido.getName(), "Fido");
+        Assert.assertEquals(fido.getFavoriteToy(), "Bone");
+        Assert.assertEquals(fido.getAge(), 5);
     }
 }
