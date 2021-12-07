d6e76e5de769bf83d99d3b3ba557779b6c494014
Mohit Kumar <mohikuma@adobe.com>
2021-12-07 18:55:09 +0530

    Sol4 - Assignment: Conditional logic exercises

--- src/test/java/com/serenitydojo/WhenFeedingTheAnimals.java
+++ src/test/java/com/serenitydojo/WhenFeedingTheAnimals.java
@@ -42,4 +42,17 @@ public class WhenFeedingTheAnimals {
         Assert.assertEquals("Salmon", food);
 
     }
+
+    @Test
+    public void shouldFeedPremiumDogsDeluxeDogFood() {
+        Feeder feeder = new Feeder();
+        String food = feeder.feeds("Dog", true);
+        Assert.assertEquals("Bone", food);
+    }
+    @Test
+    public void shouldFeedPremiumHampstersLettuce() {
+        Feeder feeder = new Feeder();
+        String food = feeder.feeds("Hampster", true);
+        Assert.assertEquals("Lettuce", food);
+    }
 }
